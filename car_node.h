fa27d93785637e7c67dfb67b6846323fb5743943
Itai Spigelman <itai10020@gmail.com>
2021-06-09 15:34:21 +0300

    comparers for sells & car nodes

--- car_node.h
+++ car_node.h
@@ -10,6 +10,22 @@
 
 struct CarNode {
     SharedPointer<CarData> carData;
+
+    bool operator<(const CarNode& other) const;
+    bool operator>=(const CarNode& other) const;
+    bool operator==(const CarNode& other) const;
 };
+
+bool CarNode::operator<(const CarNode &other) const {
+    return carData->typeID < other.carData->typeID;
+}
+
+bool CarNode::operator>=(const CarNode &other) const {
+    return !(*this<other);
+}
+
+bool CarNode::operator==(const CarNode &other) const {
+    return carData->typeID == other.carData->typeID;
+}
 
 #endif //DS_HW2_CAR_NODE_H
--- sells_node.h
+++ sells_node.h
@@ -8,6 +8,23 @@
 #include "car_data.h"
 struct SellsNode {
     SharedPointer<CarData> carData;
+
+    bool operator<(const SellsNode& other) const;
+    bool operator>=(const SellsNode& other) const;
+    bool operator==(const SellsNode& other) const;
 };
+
+bool SellsNode::operator<(const SellsNode &other) const {
+    return carData->sells < other.carData->sells ||
+            ((carData->sells == other.carData->sells) && carData->typeID < other.carData->typeID);
+}
+
+bool SellsNode::operator>=(const SellsNode &other) const {
+    return !(*this<other);
+}
+
+bool SellsNode::operator==(const SellsNode &other) const {
+    return carData->sells == other.carData->sells && carData->typeID == other.carData->typeID;
+}
 
 #endif //DS_HW2_SELLS_NODE_H
